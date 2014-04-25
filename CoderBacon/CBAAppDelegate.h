41985d3ced8fdaeded7197fc9232344e8e9f83ce
Justin Steffen <JustinDSN@gmail.com>
2014-04-24 19:34:20 -0700

    Initial Commit

---
+++ CoderBacon/CBAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CBAAppDelegate.h
+//  CoderBacon
+//
+//  Created by Justin Steffen on 4/24/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
